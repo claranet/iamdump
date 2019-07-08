6102b394a05ae5f712c9ddc887c7d510c1604c86
Raymond Butcher <ray.butcher@claranet.uk>
2019-07-08 15:36:21 +0100

    Release 0.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="iamdump",
-    version="0.0.3",
+    version="0.0.4",
     author="Raymond Butcher",
     author_email="ray.butcher@claranet.uk",
     url="https://github.com/claranet/iamdump",
